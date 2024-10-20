637b4bae99dfc7f07cd7e5f61a60fd234a3d124c
Gouri-j <gouri.j2002@gmail.com>
2024-10-20 20:09:58 +0530

    Day14 commit

---
+++ CSharpTraining2/Day14-5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_5
+{
+ 
+   delegate int operation(int a, int b);
+   internal class Program
+   {
+
+     static int Add(int a, int b)
+     {
+         return a + b;
+     }
+
+     static int Multiply(int a, int b)
+     {
+         return a * b;
+     }
+     static void Main(string[] args)
+     {
+         operation addOperation = Add;
+         operation multiplyOperation = Multiply;
+         int addResult = addOperation(10, 5);
+         Console.WriteLine($"Addition Result: {addResult}");
+         int multiplyResult = multiplyOperation(10, 5);
+         Console.WriteLine($"Multiplication Result: {multiplyResult}");
+     }
+   }
+}
