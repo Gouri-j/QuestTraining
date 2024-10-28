2925f61772d8a016300f586a71e9dbc7f3a34c74
Gouri-j <gouri.j2002@gmail.com>
2024-10-28 20:50:40 +0530

    Day20 commit

---
+++ CSharpTraining2/Day20/Cricket/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cricket
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int Runs { get; set; }
+        public int BallsFaced { get; set; }
+        public bool Isout { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Runs = 0;
+            BallsFaced = 0;
+            Isout = false;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the player name");
+            string name = Console.ReadLine();
+            var player = new Player(name);
+
+            while(player.Isout == false)
+            {
+                Console.WriteLine("Enter the runs");
+                int runs = int.Parse(Console.ReadLine());
+
+                if (runs <= 6)
+                {
+                    var random = new Random();
+                    var computer = random.Next(0, 7);
+                    Console.WriteLine($"{computer}");
+
+                    if (computer == runs)
+                    {
+                        Console.WriteLine("You are out");
+                        Console.WriteLine($"Your score : {player.Runs}");
+                        Console.WriteLine($"Number of balls faced : {player.BallsFaced}");
+                        player.BallsFaced++;
+                        player.Isout = true;
+                    } 
+                    else if (computer == 0)
+                    {
+                        Console.WriteLine("No ball");
+                        player.Runs += runs + 1;
+                        Console.WriteLine($"Score: {player.Runs}");
+
+                    }
+                    else
+                    {
+                        player.Runs += runs;
+                        Console.WriteLine($"Score: {player.Runs}");
+                        Console.WriteLine($"Ball faced : {player.BallsFaced}");
+                        player.BallsFaced++; 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+        }
+    }
+}
---
+++ CSharpTraining2/Day20/Day20/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    enum Option
+    {
+        Stone = 1,
+        Paper = 2,
+        Scissors = 3
+    }
+    class Game
+    {
+        public Option player1 { get; set; }
+        public Option player2 { get; set; }        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+           
+            int i = 0;
+            int j = 0;
+
+            while (i < 10 && j < 10)
+            {
+                Console.WriteLine("Enter the input: 1.Stone,2.Paper,3.Scissors");
+                var playerInput = Console.ReadLine();
+                var player1 = (playerInput == "1" ? Option.Stone : playerInput == "2" ? Option.Paper : Option.Scissors);
+                game.player1 = player1;
+                Console.WriteLine($"{player1}");
+
+                var random = new Random();
+                var player2 = random.Next(1, 4);
+                game.player2 = (Option)player2;
+                Console.WriteLine($"{(Option)player2}");
+
+
+                if (game.player1 == game.player2)
+                {
+                    Console.WriteLine("Tie");
+                }
+                else if (player1 == Option.Scissors && (Option)player2 == Option.Paper ||
+                        player1 == Option.Paper && (Option)player2 == Option.Stone ||
+                        player1 == Option.Stone && (Option)player2 == Option.Scissors)
+                {
+                    int markOfPlayer1 = i++;
+                    Console.WriteLine($"You scored {i}");
+                }
+                else
+                {
+                    int markOfPlayer2 = j++;
+                    Console.WriteLine($"Computer scored{j}");
+                }
+            }
+
+            if (i > j)
+            {
+                Console.WriteLine("You win");
+            }
+            else
+            {
+                Console.WriteLine("Computer win");
+            }
+        }
+    }
+}
